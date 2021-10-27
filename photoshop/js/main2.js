ac179e4280db4fe1fd7687e11d0446d528f22c85
MangMaan <91296636+Da1nJung@users.noreply.github.com>
2021-10-27 14:25:17 +0900

    2021.10.27
    photoshop html수정

---
+++ photoshop/js/main2.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+// setTimeout(function(){
+// $('.object_bg').slideDown(1000)
+// },4000)
+// $('.color').hide();
+// $('.img_wrap').mouseenter(function(){
+
+//     $(this).children('li').siblings('.color').show();
+
+
+// })/* end */
+
+
+
+
+
+
+
+
+
+})/* end */
